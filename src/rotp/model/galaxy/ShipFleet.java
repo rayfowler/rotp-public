dd8b5ff82f2970a22f7f451e27c88553a62119a0
modnar-hajile <66027258+modnar-hajile@users.noreply.github.com>
2020-10-09 10:43:16 -0500

    Bug fix: Ground bombs attacking transport gauntlet
    Fixed the issue where troop transports were being destroyed by ground bomb weapons on orbiting ships.
    
    Added firepowerAntiShip to only count weapon damage from weapons that canAttackShips. Which is then summed up to be the per turn damage for the troop transport gauntlet.

--- src/rotp/model/galaxy/ShipFleet.java
+++ src/rotp/model/galaxy/ShipFleet.java
@@ -584,6 +584,18 @@ public class ShipFleet implements Base, Sprite, Ship, Serializable {
         }
         return maxSpeed;
     }
+	// modnar: add firepowerAntiShip to only count weapons that can attack ships
+	public float firepowerAntiShip(float shield) {
+        float dmg = 0;
+        for (int i=0;i<num.length;i++) {
+            if (num[i]>0) {
+                ShipDesign des = design(i);
+                if (des != null)
+                    dmg += (num[i] * des.firepowerAntiShip(shield));
+            }
+        }
+        return dmg;
+    }
     public float firepower(float shield) {
         float dmg = 0;
         for (int i=0;i<num.length;i++) {
