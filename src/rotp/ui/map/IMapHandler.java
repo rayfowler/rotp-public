bc82bd7c02c86e4c3125ea17417df37b544e80a8
modnar-hajile <66027258+modnar-hajile@users.noreply.github.com>
2020-12-10 18:22:50 -0600

    Enable drawShield, change shield colors
    Enable drawShield code (IMapHandler.java)
    
    Change shield colors to common game loot rarity colors.
    shield-5 --> shield-10 --> shield-15 --> shield-20
    green --> blue --> purple --> orange
    
    Change the shield display to scale with map zoom.
    (change BasePanel.s15 into map.scale(0.25f))
    
    Make the shield display arc/ring slightly thicker.
    
    Change Shield-20 display into a complete circle/ring.

--- src/rotp/ui/map/IMapHandler.java
+++ src/rotp/ui/map/IMapHandler.java
@@ -92,7 +92,7 @@ public interface IMapHandler {
     default boolean showOwnerReach(StarSystem s)         { return false; }
     default boolean showOwnership(StarSystem s)          { return true; }
     default float ownerReach(StarSystem s)               { return 0; }
-    default boolean drawShield(StarSystem s)             { return false; }
+    default boolean drawShield(StarSystem s)             { return true; } // modnar: always draw shields
     default boolean shouldDrawSprite(Sprite s)           { return true; }
     default boolean canChangeMapScales()                 { return true; }
     default boolean displayNextTurnNotice()              { return false; }
