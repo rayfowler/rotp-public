46d50a806e1eae2f122a96a2fb1596a72fab8e8e
Ray Fowler <rayfowler@fastmail.com>
2021-01-25 12:39:53 -0600

    comment out code preventing nebulas from being created over home systems

--- src/rotp/model/galaxy/Galaxy.java
+++ src/rotp/model/galaxy/Galaxy.java
@@ -156,11 +156,12 @@ public class Galaxy implements Base, Serializable {
                 return false;
         }
             
+        /*
         for (EmpireSystem sys : shape.empSystems) {
             if (sys.inNebula(neb))
                 return false;
         }
-        
+        */
         nebulas.add(neb);
         return true;
     }
