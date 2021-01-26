9aefcd171402c01e288c1a2d5577fd08aae4fa82
Ray Fowler <rayfowler@fastmail.com>
2021-01-26 13:04:22 -0600

    player now receives an alert when a trade treaty reaches its maximum value

---
+++ src/rotp/ui/notifications/TradeTreatyMaturedAlert.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015-2020 Ray Fowler
+ * 
+ * Licensed under the GNU General Public License, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://www.gnu.org/licenses/gpl-3.0.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package rotp.ui.notifications;
+
+import rotp.model.game.GameSession;
+
+public class TradeTreatyMaturedAlert extends GameAlert {
+    private final int empId;
+    private final int amt;
+     public static void create(int id, int tradeAmt) {
+        GameSession.instance().addAlert(new TradeTreatyMaturedAlert(id, tradeAmt));
+    }
+    @Override
+    public String description() {
+        String name = galaxy().empire(empId).name();
+        return text("MAIN_ALERT_TRADE_MATURED", name, str(amt));
+    }
+    private TradeTreatyMaturedAlert(int id, int tradeAmt) {
+        empId = id;
+        amt = tradeAmt;
+    }
+}
