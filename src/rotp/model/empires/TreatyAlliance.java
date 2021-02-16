85737354af8d71a49499993db5911173028bf2ce
Ray Fowler <rayfowler@fastmail.com>
2021-02-15 22:14:24 -0600

    add alerts for weakening alliance

--- src/rotp/model/empires/TreatyAlliance.java
+++ src/rotp/model/empires/TreatyAlliance.java
@@ -22,6 +22,8 @@ import rotp.model.incidents.DiplomaticIncident;
 import rotp.model.incidents.EnemyAidIncident;
 import rotp.model.incidents.FinancialAidIncident;
 import rotp.model.incidents.TechnologyAidIncident;
+import rotp.ui.notifications.AllianceBreakingAlert;
+import rotp.ui.notifications.AllianceWeakeningAlert;
 import rotp.util.Base;
 
 public class TreatyAlliance extends DiplomaticTreaty implements Base {
@@ -51,6 +53,14 @@ public class TreatyAlliance extends DiplomaticTreaty implements Base {
     public void nextTurn(Empire emp)      { 
         initStandings();  // backwards-save compatibility
         int index = emp.id == empire1 ? 0 : 1;
+        int prevStanding = standings[index];
+        boolean isPlayerStanding = false;
+        switch(index) {
+            case 0: isPlayerStanding = empire2 == Empire.PLAYER_ID; break;
+            case 1: isPlayerStanding = empire1 == Empire.PLAYER_ID; break;
+        }
+
+       
         Empire emp1 = galaxy().empire(empire1);
         Empire emp2 = galaxy().empire(empire2);
         List<Empire> enemies1 = emp1.warEnemies();
@@ -84,6 +94,20 @@ public class TreatyAlliance extends DiplomaticTreaty implements Base {
             int adj = 50-maxStanding;
             standings[0] += adj;
             standings[1] += adj;
+        }
+        
+        if (isPlayerStanding) {
+            Empire nonPlayerEmpire = null;
+            switch(index) {
+                case 0: nonPlayerEmpire = emp1; break;
+                case 1: nonPlayerEmpire = emp2; break;
+            }
+            if ((prevStanding >= 0) && (standings[index] < 0)) {
+                AllianceBreakingAlert.create(nonPlayerEmpire);
+            }
+            else if ((prevStanding >= 25) && (standings[index] < 25)) {
+                AllianceWeakeningAlert.create(nonPlayerEmpire);
+            }
         }
         
         float chance = chanceBreak(index);
