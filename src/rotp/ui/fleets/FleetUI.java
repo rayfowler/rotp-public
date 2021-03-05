c8bd850edf0a360e3cafc1e44c8acc9f1fb3aaa5
Ray Fowler <rayfowler@fastmail.com>
2021-03-04 22:16:01 -0600

    fix panel cropping issue when mass deploying fleets

--- src/rotp/ui/fleets/FleetUI.java
+++ src/rotp/ui/fleets/FleetUI.java
@@ -21,7 +21,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GridLayout;
 import java.awt.LinearGradientPaint;
 import java.awt.Rectangle;
 import java.awt.Shape;
