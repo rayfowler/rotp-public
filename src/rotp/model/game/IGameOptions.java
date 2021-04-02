7d37670f7495c76ebc8290b79983192f0c821673
Ray Fowler <rayfowler@fastmail.com>
2021-04-01 21:57:18 -0500

    set default # opponents to num stars /16, rounded up. Max is still num stars /8

--- src/rotp/model/game/IGameOptions.java
+++ src/rotp/model/game/IGameOptions.java
@@ -219,6 +219,7 @@ public interface IGameOptions {
 	
     public List<String> gameDifficultyOptions();
     public int maximumOpponentsOptions();
+    public int defaultOpponentsOptions();
     public List<String> startingRaceOptions();
 
     public String selectedGalaxySize();
