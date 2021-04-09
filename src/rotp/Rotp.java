ef0dfbbfde6321a098076ce6d4a1af2b5532051d
Ray Fowler <rayfowler@fastmail.com>
2021-04-09 14:56:15 -0500

    Version 0.91

--- src/rotp/Rotp.java
+++ src/rotp/Rotp.java
@@ -43,7 +43,7 @@ public class Rotp {
     public static boolean countWords = false;
     private static String jarPath;
     private static JFrame frame;
-    public static String releaseId = "0.9";
+    public static String releaseId = "0.91";
     public static long startMs = System.currentTimeMillis();
     public static long maxHeapMemory = Runtime.getRuntime().maxMemory() / 1048576;
     public static long maxUsedMemory;
