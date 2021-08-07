e9b7003f28dea14783f8ad86a219d291b2bc10fd
Xilmi <ail.st@gmx.de>
2021-08-07 18:11:55 +0200

    No security-spending when no contact. But luckily that actually makes no difference between it only actually gets detracted from income when there's contacts.

--- src/rotp/model/ai/xilmi/AISpyMaster.java
+++ src/rotp/model/ai/xilmi/AISpyMaster.java
@@ -49,9 +49,11 @@ public class AISpyMaster implements Base, SpyMaster {
             }
         }
         paranoia = empire.tech().avgTechLevel() - highestOpponentTechLevel;
+        if(highestOpponentTechLevel == 0)
+            paranoia = 0;
         if (paranoia < 0)
             paranoia = 0;
-        //System.out.println(empire.galaxy().currentTurn()+" "+ empire.name()+" counter-espionage: "+paranoia+" mt: "+empire.tech().avgTechLevel()+" ot: "+avgOpponentTechLevel+" mct: "+empire.tech().computer().techLevel()+" pct: "+avgOpponentComputerLevel);
+        //System.out.println(empire.galaxy().currentTurn()+" "+ empire.name()+" counter-espionage: "+paranoia+" mt: "+empire.tech().avgTechLevel()+" ot: "+highestOpponentTechLevel);
         return min(10, (int)Math.round(paranoia)); // modnar: change max to 10, MAX_SECURITY_TICKS = 10
     }
     @Override
