336e906006a16c2141c7580064f4b69a316298f6
Ray Fowler <rayfowler@fastmail.com>
2021-09-28 15:17:52 -0500

    fix bug introduced with retreat canceling feature that breaks when you retreat from a monster combat

--- src/rotp/model/combat/ShipCombatManager.java
+++ src/rotp/model/combat/ShipCombatManager.java
@@ -425,16 +425,18 @@ public class ShipCombatManager implements Base {
         
         // cancel the retreat of any ships that belong to the combat victor
         Empire victor = results.victor();
-        List<ShipDesign> retreatedShips = new ArrayList<>(results.shipsRetreated().keySet());
-        boolean retreatsCancelled = false;
-        for (ShipDesign des: retreatedShips) {
-            if (victor.shipLab().design(des.id()) == des) {
-                results.shipsRetreated().remove(des);
-                retreatsCancelled = true;
-            }        
+        if (victor != null) {
+            List<ShipDesign> retreatedShips = new ArrayList<>(results.shipsRetreated().keySet());
+            boolean retreatsCancelled = false;
+            for (ShipDesign des: retreatedShips) {
+                if (victor.shipLab().design(des.id()) == des) {
+                    results.shipsRetreated().remove(des);
+                    retreatsCancelled = true;
+                }        
+            }
+            if (retreatsCancelled)
+                galaxy().ships.cancelRetreatingFleets(victor.id, system().id);
         }
-        if (retreatsCancelled)
-            galaxy().ships.cancelRetreatingFleets(victor.id, system().id);
         
         // ensure rebels are killed in proportionn to overall population
         results.killRebels();
