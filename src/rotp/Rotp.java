6eaa2d55a9607c44ba5c1a742ff72e04625b760a
Xilmi <ail.st@gmx.de>
2021-10-20 19:54:23 +0200

    Implemented Home- and End-Hotkey in Ship-Component selection so you can scroll down the weapons-list with one button.

--- src/rotp/Rotp.java
+++ src/rotp/Rotp.java
@@ -44,7 +44,7 @@ public class Rotp {
     public static boolean countWords = false;
     private static String startupDir;
     private static JFrame frame;
-    public static String releaseId = "0.95c";
+    public static String releaseId = "0.95d";
     public static long startMs = System.currentTimeMillis();
     public static long maxHeapMemory = Runtime.getRuntime().maxMemory() / 1048576;
     public static long maxUsedMemory;
